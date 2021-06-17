45b1d2f6dec49b84552a2593edff37ff090944cf
rasmushyldahl <rasmushyldahl@hotmail.com>
2021-06-17 13:38:17 +0200

    Adding telstar logo to base project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/TelstarLogo.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.erdetfredag.dk/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter application
         </a>
       </header>
     </div>
